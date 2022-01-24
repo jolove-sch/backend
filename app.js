c88e14d86dc9b7a291cdc6940bc6b581cf80e272
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-01-24 23:13:01 +0900

    initial commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
