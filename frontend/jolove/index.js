af9b704e09ba9f320b8554a1433b8d968ac93aa8
subinK <76421788+ksubin98@users.noreply.github.com>
2022-02-08 15:25:03 +0900

    Add files via upload

---
+++ frontend/jolove/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import User from './redux/module/user';
+
+const user = new User();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App user={user} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
